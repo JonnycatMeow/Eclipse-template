201978f33346bc40c1d826cb85dd7c522a78f5a5
Jonathan Gomez <107599365+JonnycatMeow@users.noreply.github.com>
2023-10-06 00:05:19 -0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,6 +1,6 @@
-{ pkgs }: {
+{pkgs}: {
 	deps = [
-		pkgs.nodejs-18_x
+	pkgs.nodejs-18_x
         pkgs.nodePackages.typescript-language-server
         pkgs.nodePackages.yarn
         pkgs.replitPackages.jest
